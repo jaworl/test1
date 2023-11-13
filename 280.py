f9a825190fe1f883784218f4d0555a9b30afa40b
jaworl <149052746+jaworl@users.noreply.github.com>
2023-11-13 21:59:18 +0330

    Update 280.py

--- 280.py
+++ 280.py
@@ -1,10 +1,1 @@
-x = int(input())
-y = int(input())
-z = int(input())
-if (x != 0 and y != 0 and z != 0) :
-    if ((x * x) + (y * y) == (z * z) or (x * x) + (z * z) == (y * y) or (y * y) + (z * z) == (x * x)) :
-        print("Yes")
-    else :
-        print("No")
-else :
-    print("No")
+# test
