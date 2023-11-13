39f23bbbeaf6fae0aa48cdc5d2ce80b120041718
jaworl <149052746+jaworl@users.noreply.github.com>
2023-11-13 21:54:59 +0330

    Update Home.py

--- View/Home.py
+++ View/Home.py
@@ -12,15 +12,31 @@
 
 
 
-def SignUp() :
-    name = input("enter student's name: ")
-    lastname = input("lastname: ")
-    id = input("id: ")
-    classroom = input("classroom: ")
-    address = input("address: ")
-    postcode = input("postcode: ")
-    telephone = input("telephone: ")
-    dishis = input("diseases history: ")
-    moadel = input("moadel: ")
-    fathername = input("father's name: ")
-SignUp()
+# def SignUp() :
+#     name = input("enter student's name: ")
+#     lastname = input("lastname: ")
+#     id = input("id: ")
+#     classroom = input("classroom: ")
+#     address = input("address: ")
+#     postcode = input("postcode: ")
+#     telephone = input("telephone: ")
+#     dishis = input("diseases history: ")
+#     moadel = input("moadel: ")
+#     fathername = input("father's name: ")
+# SignUp()
+
+from Control import all_id
+
+def show ():
+    menu () 
+    if menu() == 1 :
+        TorS = int(input("1.you are a teacher/n2.you are a student"))
+        if TorS == 1 :
+            teachers_reagester()
+        if TorS == 2 :
+            students_register()
+    if menu() == 2 :
+        get_id = int(input("enter your id to log in : "))
+        if get_id in all_id :
+            print()
+show()
