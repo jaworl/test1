380f7f45bb94ec50a25160b6433d285550a6f3fd
jaworl <149052746+jaworl@users.noreply.github.com>
2023-11-13 21:47:07 +0330

    Update Grades.py

--- Model/Grades.py
+++ Model/Grades.py
@@ -1,6 +1,6 @@
 import Model.Exam
 class Grades :
-    def __init__ (self) :
+    def __init__ (self , name , lName) :
         # self.examResults = []
         self.name = name
         self.lName = lName
@@ -26,7 +26,6 @@ class Grades :
     def get_geology(self) :
         return self.chemistry
     def get_arabic(self) :
-        def get_arabic(self) :
         return self.arabic
     def get_history(self) :
         return self.history
@@ -34,9 +33,26 @@ class Grades :
         return self.english
     def get_dini(self) :
         return self.dini
+        
         #continuio
-    def addToExam(exam) :
-        if exam == "math" :
+    def set_math(self,math):
+        self.math = math
+    def set_biology(self,biology):
+        self.biology = biology
+    def set_physics(self,physics):
+        self.physics = physics
+    def set_chemistry(self,chemistry):
+        self.chemistry = chemistry
+    def set_geology(self,geology):
+        self.geology = geology
+    def set_arabic(self,arabic):
+        self.arabic = arabic
+    def set_history(self,history):
+        self.history = history
+    def set_english(self.english):
+        self.english = english
+    def set_dini(self,dini):
+        self.dini = dini
             
     def add_exam_result(self , examRes) :
         self.examResults.append(examRes)
