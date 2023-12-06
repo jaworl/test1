6d4a8fca91f9551ef4b88233aae1c719ee8a85c3
jaworl <149052746+jaworl@users.noreply.github.com>
2023-12-06 21:47:48 +0330

    Update Home.py

--- View/Home.py
+++ View/Home.py
@@ -2,29 +2,7 @@ import Model.Teacher
 import Model.Student
 import Control.Teacher
 import Control.Student
-ids=0
-studentInClass = [0]
-def SignUpStu() :
-     global all_members
-     global id
-     global studentInClass
-     global all_id
-     print("answer the questions below to complete youre registeration : ")
-     name = input("enter student's name: ")
-     lastname = input("lastname: ")
-     classroom = input("classroom: ")
-     address = input("address: ")
-     postcode = input("postcode: ")
-     telephone = input("telephone: ")
-     dishis = input("diseases history: ")
-     moadel = input("moadel: ")
-     fathername = input("father's name: ")
-     studentInClass.append(name + lastname)
-     all_members.append(name + lastname)
-     id = all_members.index(name + lastname)
-     info = Model.Student.Student(name,lastname,classroom,address,postcode,telephone,dishis,moadel,fathername , id)
-     print ("you have been registerd with these information : ")
-     print(studentInClass)
+
 
 def LogIn() :
         global teacherInClass
