fab7c905ecd9c26f70b14240bcc7242d2921bdcc
nfaskri <149053204+nfaskri@users.noreply.github.com>
2023-12-14 01:45:21 -0800

    Delete SCHOOL directory

--- SCHOOL/Control/control_login.py
+++
@@ -1,8 +0,0 @@
-
-def show() :
-    f = open("C:/Users/ASUS/Desktop/school/studentinfo.txt", "w")
-    lines = f.readlines()
-    f.close()
-    print(lines)
-    for line in lines :
-        print(line)
